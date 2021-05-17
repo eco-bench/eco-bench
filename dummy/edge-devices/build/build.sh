dfc642134a9ba51958263c23bce6fc20c2d05ec4
SeveHo <severin@hoegl.org>
2021-05-17 15:04:35 +0200

    Update build.sh

--- dummy/edge-devices/build/build.sh
+++ dummy/edge-devices/build/build.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-DEVICE_TYPE="camera"
-
-
 echo $PAT | docker login ghcr.io --username phanatic --password-stdin
 
 VERSION="1"
 docker build \
-    --tag ghcr.io/eco-bench/edge-${DEVICE_TYPE}:${VERSION} . # run the script from the project root
+    --tag ghcr.io/eco-bench/edge-device:${VERSION} . # run the script from the project root
 
-echo [INFO] Pushin to ghcr.io/eco-bench/edge-${DEVICE_TYPE}:${VERSION}
+echo [INFO] Pushin to ghcr.io/eco-bench/edge-device:${VERSION}
 
-docker push ghcr.io/eco-bench/edge-${DEVICE_TYPE}:${VERSION}
+docker push ghcr.io/eco-bench/edge-device:${VERSION}
