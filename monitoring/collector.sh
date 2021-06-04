9a8a941815f15d967632bae8a4e4bbb58f3d57c2
Backerich <25672920+Backerich@users.noreply.github.com>
2021-06-04 20:19:39 +0200

    Draft monitoring script for cpu and memory

---
+++ monitoring/collector.sh
@@ -0,0 +1,16 @@
+# CPU and Memory:
+while true; do
+    timestamp=$(printf '%s' "$(date)");
+    cpu_usage=$(top -b -d1 -n1|grep -i "Cpu(s)"|head -c21|cut -d ' ' -f3|cut -d '%' -f1);
+    memory_all=$(top -b | head -4 | tail -1);
+    memory_total=$(echo $memory_all | head -c16 | tail -c6);
+    memory_free=$(echo $memory_all | head -c29 | tail -c6);
+    memory_used=$(echo $memory_all | head -c43 | tail -c6);
+    # memory_percentage=$(100 * ($memory_used / $memory_total));
+    echo "$timestamp; CPU:$cpu_usage; MEM_TOTAL:$memory_total; MEM_FREE: $memory_free; MEM_USED: $memory_used" >> stats.log;
+    sleep 2; 
+done
+
+# watch -n 2
+# top -b | head -4 | tail -2 | perl -anlE 'say sprintf("used: %s   total: %s  => RAM Usage: %.1f%%", \$F[7], \$F[3], 100*\$F[7]/\$F[3]) if /MiB Mem/'
+# grep -E --color 'Mem|Cache|Swap' /proc/meminfo
