6f55275a9a0b683c5d696ada1b03900a611fdb40
Backerich <25672920+Backerich@users.noreply.github.com>
2021-06-04 22:38:08 +0200

    Rename of output file to .json

--- monitoring/collector.sh
+++ monitoring/collector.sh
@@ -4,7 +4,7 @@
 max_seconds=$1
 SECONDS=0
 
-echo "{'logs': [" >> stats.log
+echo "{'logs': [" >> stats.json
 while [ $SECONDS -le $max_seconds ]; do
     timestamp=$(printf '%s' "$(date)");
     echo $SECONDS
@@ -15,10 +15,10 @@ while [ $SECONDS -le $max_seconds ]; do
     memory_free=$(echo $memory_all | head -c29 | tail -c6);
     memory_used=$(echo $memory_all | head -c43 | tail -c6);
     # memory_percentage=$(100 * ($memory_used / $memory_total));
-    echo "{ 'timestamp': '$timestamp', 'CPU': '$cpu_usage', 'MEM_TOTAL': '$memory_total', 'MEM_FREE': '$memory_free', 'MEM_USED': '$memory_used'}," >> stats.log;
+    echo "{ 'timestamp': '$timestamp', 'CPU': '$cpu_usage', 'MEM_TOTAL': '$memory_total', 'MEM_FREE': '$memory_free', 'MEM_USED': '$memory_used'}," >> stats.json;
     sleep 2;
 done;
-echo "]}" >> stats.log
+echo "]}" >> stats.json
 # watch -n 2
 # top -b | head -4 | tail -2 | perl -anlE 'say sprintf("used: %s   total: %s  => RAM Usage: %.1f%%", \$F[7], \$F[3], 100*\$F[7]/\$F[3]) if /MiB Mem/'
 # grep -E --color 'Mem|Cache|Swap' /proc/meminfo
