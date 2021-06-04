c476250e8f6c02beb02c1407dbd48b110e66bcc3
Backerich <25672920+Backerich@users.noreply.github.com>
2021-06-04 22:36:26 +0200

    Select time intervall in arguments and now outputs json

--- monitoring/collector.sh
+++ monitoring/collector.sh
@@ -1,6 +1,13 @@
+#!/usr/bin/env bash
+
 # CPU and Memory:
-while true; do
+max_seconds=$1
+SECONDS=0
+
+echo "{'logs': [" >> stats.log
+while [ $SECONDS -le $max_seconds ]; do
     timestamp=$(printf '%s' "$(date)");
+    echo $SECONDS
     cpu_usage=$(top -b -d1 -n1|grep -i "Cpu(s)"|head -c13|tail -c5);
     # |cut -d ' ' -f3|cut -d '%' -f1);
     memory_all=$(top -b | head -4 | tail -1);
@@ -8,10 +15,10 @@ while true; do
     memory_free=$(echo $memory_all | head -c29 | tail -c6);
     memory_used=$(echo $memory_all | head -c43 | tail -c6);
     # memory_percentage=$(100 * ($memory_used / $memory_total));
-    echo "$timestamp; CPU:$cpu_usage; MEM_TOTAL:$memory_total; MEM_FREE: $memory_free; MEM_USED: $memory_used" >> stats.log;
-    sleep 2; 
-done
-
+    echo "{ 'timestamp': '$timestamp', 'CPU': '$cpu_usage', 'MEM_TOTAL': '$memory_total', 'MEM_FREE': '$memory_free', 'MEM_USED': '$memory_used'}," >> stats.log;
+    sleep 2;
+done;
+echo "]}" >> stats.log
 # watch -n 2
 # top -b | head -4 | tail -2 | perl -anlE 'say sprintf("used: %s   total: %s  => RAM Usage: %.1f%%", \$F[7], \$F[3], 100*\$F[7]/\$F[3]) if /MiB Mem/'
 # grep -E --color 'Mem|Cache|Swap' /proc/meminfo
