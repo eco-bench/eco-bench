73b8a0d7f1c3f5cbd711e38d09b7452ca6f6a817
Backerich <25672920+Backerich@users.noreply.github.com>
2021-06-13 13:57:24 +0200

    collector script changes

--- benchmarking/collector.sh
+++ benchmarking/collector.sh
@@ -5,18 +5,19 @@ max_seconds=$1
 # Mongo DB Endpoint
 # user=testUser
 # password=passw0rd
-host_ip=10.128.0.29
+host_ip=10.128.0.2
 database=metrics
 SECONDS=0
 
 "" > stats.json
-echo "db.metrics_collection_new.insertMany([" >> stats.json
+eco_name=$2
+echo "db.$eco_name.insertMany([" >> stats.json
 
 while [ $SECONDS -le $max_seconds ]; do
     # Empty Stats File
     # "" > stats.json
     timestamp=$(printf '%s' "$(date)");
-    hostname=$(echo hostname);
+    hostname=$(echo $(hostname));
     cpu_usage=$(top -b -d1 -n1|grep -i "Cpu(s)"|head -c13|tail -c5);
     # |cut -d ' ' -f3|cut -d '%' -f1);
     memory_all=$(top -b | head -4 | tail -1);
@@ -24,12 +25,12 @@ while [ $SECONDS -le $max_seconds ]; do
     memory_free=$(echo $memory_all | head -c29 | tail -c6);
     memory_used=$(echo $memory_all | head -c43 | tail -c6);
     io_all=$(iotop -b -n 1 -o);
-    io_total_read=$(echo $io_all |grep -e Total | awk '{print $4}');
-    io_total_write=$(echo $io_all |grep -e Total | awk '{print $10}');
-    io_current_read=$(echo $io_all |grep -e Current | awk '{print $15}')
-    io_current_write=$(echo $io_all |grep -e Current | awk '{print $21}')
+    io_total_read=$(echo $io_all |grep -e Total | awk '{print $5}');
+    io_total_write=$(echo $io_all |grep -e Total | awk '{print $12}');
+    io_current_read=$(echo $io_all |grep -e Actual | awk '{print $17}')
+    io_current_write=$(echo $io_all |grep -e Actual | awk '{print $23}')
     # memory_percentage=$(100 * ($memory_used / $memory_total));
-    echo "{ 'timestamp': '$timestamp', 'CPU': '$cpu_usage', 'MEM_TOTAL': '$memory_total', 'MEM_FREE': '$memory_free', 'MEM_USED': '$memory_used', 'FIO_TOTAL_READ': '$io_total_read', 'FIO_TOTAL_WRITE': '$io_total_write', 'FIO_CURRENT_READ': '$io_current_read', 'FIO_CURRENT_WRITE': '$io_current_write'}," >> stats.json;
+    echo "{ 'HOST': '$hostname', 'timestamp': '$timestamp', 'CPU': '$cpu_usage', 'MEM_TOTAL': '$memory_total', 'MEM_FREE': '$memory_free', 'MEM_USED': '$memory_used', 'FIO_TOTAL_READ': '$io_total_read', 'FIO_TOTAL_WRITE': '$io_total_write', 'FIO_CURRENT_READ': '$io_current_read', 'FIO_CURRENT_WRITE': '$io_current_write'}," >> stats.json;
     sleep 2;
 done;
 echo "])" >> stats.json
