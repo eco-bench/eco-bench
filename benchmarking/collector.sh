84c3f634ed25bacc739db95b5f31ee26b49faad4
Backerich <25672920+Backerich@users.noreply.github.com>
2021-06-16 14:21:58 +0200

    script fix

--- benchmarking/collector.sh
+++ benchmarking/collector.sh
@@ -14,16 +14,16 @@ while [ $SECONDS -le $max_seconds ]; do
     hostname=$(echo $(hostname));
     cpu_usage=$(top -b -d1 -n1 |grep -i "Cpu(s)" |awk '{print $2}')
 
-    memory_all=$(top -b -d1 -n1 |grep "KiB Mem")
+    memory_all=$(top -b -d1 -n1 |grep "MiB Mem")
     memory_total=$(echo $memory_all |awk '{print $4}');
     memory_free=$(echo $memory_all |awk '{print $6}');
     memory_used=$(echo $memory_all |awk '{print $8}');
 
     io_all=$(iotop -b -n 1 -o);
-    io_total_read=$(echo $io_all |grep -e Total | awk '{print $5}');
-    io_total_write=$(echo $io_all |grep -e Total | awk '{print $12}');
-    io_current_read=$(echo $io_all |grep -e Actual | awk '{print $17}')
-    io_current_write=$(echo $io_all |grep -e Actual | awk '{print $23}')
+    io_total_read=$(echo $io_all |grep -e Total | awk '{print $4}');
+    io_total_write=$(echo $io_all |grep -e Total | awk '{print $10}');
+    io_current_read=$(echo $io_all |grep -e Current | awk '{print $4}')
+    io_current_write=$(echo $io_all |grep -e Current | awk '{print $10}')
 
     # memory_percentage=$(100 * ($memory_used / $memory_total));
     echo "{ 'HOST': '$hostname', 'timestamp': '$timestamp', 'CPU': '$cpu_usage', 'MEM_TOTAL': '$memory_total', 'MEM_FREE': '$memory_free', 'MEM_USED': '$memory_used', 'FIO_TOTAL_READ': '$io_total_read', 'FIO_TOTAL_WRITE': '$io_total_write', 'FIO_CURRENT_READ': '$io_current_read', 'FIO_CURRENT_WRITE': '$io_current_write'}," >> stats.json;
