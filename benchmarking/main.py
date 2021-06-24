a37e8fff1b3a5c84cd929427a84a6d415395a864
Backerich <25672920+Backerich@users.noreply.github.com>
2021-06-24 18:00:34 +0200

    Seaborn and boxplots

--- benchmarking/main.py
+++ benchmarking/main.py
@@ -1,13 +1,13 @@
 import numpy as np
-import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
 import json
 from datetime import datetime
-from pandas import DataFrame
 from pymongo import MongoClient
+import seaborn as sns
+sns.set_style("whitegrid")
 
-data_path = "../../benchmarking/data/"
+data_path = "./data/"
 
 # def get_data_from_mongodb(eco):
 #     client = MongoClient(host='35.192.54.171:27017')
@@ -15,21 +15,25 @@ data_path = "../../benchmarking/data/"
 #     collection = db[eco]
 #     val = collection.find()
 
-def benchmarking_plot(title, attribute):
-    data = data_for_plot(open(data_path + "microk8s-application.json").read(), attribute, True)
-    data2 = data_for_plot(open(data_path + "k3s-application.json").read(), attribute, False)
+def benchmarking_plot(title, attribute, yLabel, boxplot=False):
+    data = data_for_plot(open(data_path + "microk8s-idle.json").read(), attribute, True, 'microk8s')
+    data2 = data_for_plot(open(data_path + "k3s-idle.json").read(), attribute, False, 'k3s')
+    ax = None
 
-    plt.plot(data['time'], data['value'], label="microk8s")
-    plt.plot(data2['time'], data2['value'], label="k3s")
+    if boxplot:
+        data3 = pd.concat([data, data2])
+        ax = sns.boxplot(x="eco", y="value", data=data3)
+        ax.set(xlabel='Eco')
+    else:
+        ax = sns.lineplot(data=data, x='time', y='value', label="mircok8s")
+        sns.lineplot(data=data2, x='time', y='value', label="k3s")
+        plt.legend()
+        ax.set(xlabel='Time in Seconds')
 
-    plt.xticks(range(0,30))
-    plt.xticks(rotation=90)
-
-    plt.title(title)
-    plt.legend()
+    ax.set(ylabel=yLabel, title=title)
     plt.show()
 
-def data_for_plot(json_data, attribute, calc):
+def data_for_plot(json_data, attribute, calc, eco):
     parsed_json = (json.loads(json_data))
     values = []
     timestamps = []
@@ -46,13 +50,12 @@ def data_for_plot(json_data, attribute, calc):
 
         values.append(att)
 
-    data = {'value': values,
-            'time': timestamps}
+    return pd.DataFrame({'time': timestamps, 'value': values, 'eco': [eco] * len(values)})
 
-    return data
 
 if __name__ == '__main__':
-    benchmarking_plot('CPU over time', 'CPU')
-    benchmarking_plot('Memory usage over time', 'MEM_USED')
-    benchmarking_plot('File IO total read', 'FIO_TOTAL_READ')
-    benchmarking_plot('Fiel IO total write', 'FIO_TOTAL_WRITE')
+    benchmarking_plot('CPU over time', 'CPU', 'CPU in Percentage')
+    benchmarking_plot('CPU over time', 'CPU', 'CPU in Percentage', boxplot=True)
+    # benchmarking_plot('Memory usage over time', 'MEM_USED', 'Mem in MiB')
+    # benchmarking_plot('File IO total read', 'FIO_TOTAL_READ', 'Reads in Percentage')
+    # benchmarking_plot('Fiel IO total write', 'FIO_TOTAL_WRITE', 'Writes in Percentage')
