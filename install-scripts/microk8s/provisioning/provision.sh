6bc5fa817b2a8a7e9ea0e2f710393a7704cc41ff
Lucas Dario Stocksmeier <lucas@stocksmeier.de>
2021-06-29 22:02:55 +0200

    change provision without cluster state

--- install-scripts/microk8s/provisioning/provision.sh
+++ install-scripts/microk8s/provisioning/provision.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 terraform init \
-&& terraform apply -var-file tfvars.json -state cluster.tfstate -auto-approve
+&& terraform apply -var-file tfvars.json -auto-approve
 
 bash ./generate-inventory.sh cluster.tfstate > ../hosts.ini
