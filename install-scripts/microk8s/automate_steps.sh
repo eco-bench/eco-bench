1ecaab6e1cf516bcae6499ca773f9d3182e731c1
Lucas Dario Stocksmeier <lucas@stocksmeier.de>
2021-06-30 09:38:13 +0200

    created README.md + updated automate_steps.sh

--- install-scripts/microk8s/automate_steps.sh
+++ install-scripts/microk8s/automate_steps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd provisioning && ./provision.sh
-cd .. && ansible-playbook -i hosts.ini ansible/cluster_install.yml
+cd .. && ansible-playbook -i provisioning/inventory.ini ansible/cluster_install.yml
 ansible-playbook -i hosts.ini ansible/apply_applications.yml
 echo 'Sleep for 600 Seconds'
 sleep 600
